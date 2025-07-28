6b4b9b4975586bf20c8727d2eb93c3a1e24640bb
aamirnaqvi200-cpu <aamirnaqvi200@gmail.com>
2025-07-28 12:10:25 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -169,7 +169,7 @@ className="relative min-h-screen w-full overflow-hidden bg-transparent"
           style={{ bottom: '5%' }}
         >
           <div 
-            className="text-[4rem] md:text-[10rem] lg:text-[8rem] font-bold text-black/40 select-none leading-none opacity-0 animate-fade-in-delayed"
+            className=" font-bold text-black/40 select-none leading-none opacity-0 animate-fade-in-delayed"
             style={{ width: '800px',
                 height: '400px',
               animationDelay: '0.1s',  
