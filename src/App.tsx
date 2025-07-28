902faa30dc1327b03c42bba5d668c237444f7402
aamirnaqvi200-cpu <aamirnaqvi200@gmail.com>
2025-07-28 11:05:15 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -115,8 +115,8 @@ className="relative min-h-screen w-full overflow-hidden bg-transparent"
               style={{ top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: '705px', // You can change size
-    height: '705px', animationDelay: '0.3s', animationFillMode: 'forwards' }}
+    width: '755px', // You can change size
+    height: '755px', animationDelay: '0.3s', animationFillMode: 'forwards' }}
             > 
               <img 
                 src="/public/me.png"
