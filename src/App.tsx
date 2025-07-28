7c95134e8afdcf15ef5481c4e49951ffcdb1bcec
aamirnaqvi200-cpu <aamirnaqvi200@gmail.com>
2025-07-28 11:58:47 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const testimonialBadges: TestimonialBadge[] = [
   { word: "PROFOUND", rating: 5, attribution: "— AIGA", position: { top: "47%", left: "84%" }, delay: 3.3 },
   { word: "STUNNING", rating: 5, attribution: "— Vogue", position: { top: "12%", left: "10%" }, delay: 2.7 },
   { word: "REVOLUTIONARY", rating: 5, attribution: "— Wired", position: { top: "40%", left: "2%" }, delay: 2.0 },
-  { word: "CAPTIVATING", rating: 5, attribution: "— Elle", position: { top: "45%", left: "63%" }, delay: 3.9 },
+  { word: "CAPTIVATING", rating: 5, attribution: "— Elle", position: { top: "45%", left: "56%" }, delay: 3.9 },
     { word: "CREATIVE", rating: 5, attribution: "— Inkwellmedia", position: { top: "55%", left: "73%" }, delay: 3.9 }
 ];
 
